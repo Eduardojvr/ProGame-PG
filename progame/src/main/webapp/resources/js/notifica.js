5547297a4f9026f14e65e6de711168204c37c292
Eduardojvr <eduardojvr10@gmail.com>
2019-12-12 14:41:06 -0300

    Modifica tabela de desafios recebidos

--- progame/src/main/webapp/resources/js/notifica.js
+++ progame/src/main/webapp/resources/js/notifica.js
@@ -1,68 +1,24 @@
 new Vue({
-    el:'#notifica',
-    vuetify: new Vuetify(),
-  data () {
-    return {
-      perPage: 5,
-      currentPage: 1,
-      search: '',
-      headers: [
-            {
-              align: 'left',
-              sortable: false,
-            },
-            { key: 'idDesafio', label: 'Prioridade Desafio' },
-            { key: 'tituloDesafio', label: 'Título' },
-            { key: 'desafio', label: 'Desafio' },
-            { key: 'respostaDesafiado', label: 'Sua resposta' },
-            { key: 'resultado', label: 'Resultado' } 
-       ],
-      desafios:[
-          {
-              desafio: '',
-              idDesafio: '',
-              matriculaDesafiado: '',
-              matriculaDesafiante: '',
-              respostaDesafiado: '',
-              resultado: '',
-              tituloDesafio:''
+  el:'#notifica',
+  data: {
+    totalvs: 0,
+    totalNotificacao: 0
+  },
+  created: function(){
+    const vm = this;
+    var count = 0;
+    axios.get('../rs/user/getUser').then(function(response){
+      axios.get('../rs/user/todosDesafios/'+response.data["matricula"]).then(function(response){
+          while(response.data[count]){
+              count+=1;
           }
-      ],
-      totalNotificacao: 0
+          vm.totalvs = count;
+          vm.totalNotificacao = 1;
+      });
+  });
+  }
 
-    }
-  },
-  computed: {
-    rows() {
-      return this.desafios.length
-    }
-  },
-    created: function(){
-        const vm = this;
-        var count = 0;
-        axios.get('../rs/user/getUser').then(function(response){
-            axios.get('../rs/user/todosDesafios/'+response.data["matricula"]).then(function(response){
-                while(response.data[count]){
-                    if(response.data[count]['resultado'] == '' || response.data[count]['resultado'] == null){
-                        response.data[count]['resultado'] = 'Aguardando correção';
-                    }
 
-                    if(response.data[count]['respostaDesafiado'] == '' || response.data[count]['respostaDesafiado'] == null){
-                        response.data[count]['respostaDesafiado'] = 'Sem resposta';
-                    }
-                    response.data[count]["idDesafio"] = count+1;
-                    // vm.desafios.push(response.data[count]);
-                    count+=1;
-                }
-                vm.totalNotificacao = count;
-                vm.desafios = response.data;
-            });
-        });
-    },
-    methods: {
-        responder: function(){
-            alert();
-        }
-    }
+
+
 });
-
